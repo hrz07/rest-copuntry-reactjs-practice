699cfc0fcdada4a833be5f3a5eb2e2ff8f62a29a
hrz07 <hridoy.developer@yahoo.com>
2022-03-17 15:33:03 +0600

    card design complete

--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const Country = ({ countryData }) => {
-    console.log(countryData.name.common)
 
     return (
         <div>
             <div className="card my-2" style={{width: '18rem'}}>
                 <img src={countryData.flags.png} className="card-img-top" alt="..." />
-                    <div className="card-body">
+                    <div className="card-body shadow-lg">
                         <h4 className="card-title fw-bold text-danger">{countryData.name.common}</h4>
                         <p className="card-text m-0 p-0">Capital City : {countryData.capital}</p>
                         <p className="card-text m-0 p-0">Population : {countryData.population}</p>
                         <p className="card-text m-0 p-0">Continents : {countryData.continents}</p>
+                        <p className="card-text m-0 p-0">Area : {countryData.area}</p>
+            
                        
                     </div>
             </div>
